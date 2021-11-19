0bf99e849c6e0d00462ee3f787e0c04c00fd860f
darine <benmahmouddarine@gmail.com>
2021-11-19 16:00:29 +0100

    newwww

--- server/routes/reservationRoutes.js
+++ server/routes/reservationRoutes.js
@@ -1,18 +1,18 @@
 
-const express = require('express')
-const authMiddleware = require ('../middleware/authMiddleware')
-const router = express.Router()
-const {getreservations,getreservationsbySalle, addreservations , getreservationsbyId ,updateReservation, deleteReservation} = require('../Controllers/reservationController.js')
-const {getsalles ,getsallesbydept,updateSalle,deleteSalle,addSalle} = require('../Controllers/sallesController.js')
-router.get('/getreservations',authMiddleware,getreservations)
-router.post('/addreservations',authMiddleware,addreservations)
-router.get('/getsalles',authMiddleware,getsalles)
-router.get('/getsallesbydept/:dep',authMiddleware,getsallesbydept)
-router.get('/getreservationsbyId/:id',authMiddleware,getreservationsbyId)
-router.post('/addSalle',authMiddleware,addSalle)
-router.delete('/deleteReservation/:id',authMiddleware,deleteReservation)
-router.put('/updateReservation/:id',authMiddleware,updateReservation)
-router.get('/getreservationsbySalle/:id',authMiddleware,getreservationsbySalle)
-router.put('/updateSalle/:id',authMiddleware,updateSalle)
-router.delete('/deleteSalle/:id',authMiddleware,deleteSalle)
+const express = require('express');
+const authMiddleware = require ('../middleware/authMiddleware');
+const router = express.Router();
+const {getreservations,getreservationsbySalle, addreservations , getreservationsbyId ,updateReservation, deleteReservation} = require('../Controllers/reservationController.js');
+const {getsalles ,getsallesbydept,updateSalle,deleteSalle,addSalle} = require('../Controllers/sallesController.js');
+router.get('/getreservations',authMiddleware,getreservations);
+router.post('/addreservations',authMiddleware,addreservations);
+router.get('/getsalles',authMiddleware,getsalles);
+router.get('/getsallesbydept/:dep',authMiddleware,getsallesbydept);
+router.get('/getreservationsbyId/:id',authMiddleware,getreservationsbyId);
+router.post('/addSalle',authMiddleware,addSalle);
+router.delete('/deleteReservation/:id',authMiddleware,deleteReservation);
+router.put('/updateReservation/:id',authMiddleware,updateReservation);
+router.get('/getreservationsbySalle/:id',authMiddleware,getreservationsbySalle);
+router.put('/updateSalle/:id',authMiddleware,updateSalle);
+router.delete('/deleteSalle/:id',authMiddleware,deleteSalle);
 module.exports = router
